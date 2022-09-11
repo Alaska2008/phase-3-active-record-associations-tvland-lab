d60664d4f0678b97ec3946951556b1338f68c367
Amen Atikpo <alaskale2024@gmail.com>
2022-09-11 13:56:01 +0000

    commit tree 9e24c8d509fc613adc70fcaeb9778572beda2120

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,14 @@
 class Character < ActiveRecord::Base
+    belongs_to :actor
+    belongs_to :show
   
+    def actor
+        self.actor.characters
+    end
+    def show
+        #self.show.characters
+    end
+    def say_that_thing_you_say
+
+    end
 end
